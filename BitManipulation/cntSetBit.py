5c30f7d9087c7252291b710ce33f6215516445e7
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-03-01 22:19:00 +0530

    Implement second approach for counting set bits
    Added a second approach to count set bits in a number.

--- BitManipulation/cntSetBit.py
+++ BitManipulation/cntSetBit.py
@@ -9,3 +9,15 @@ def cnt(n):
     n=n>>1
   return c
 print(cnt(n))
+
+#2nd approach
+
+n=int(input())
+def cnt(n):
+  c=0
+  while n>0:
+    if n&1==1:
+      c+=1
+    n=n>>1
+  return c
+print(cnt(n))
