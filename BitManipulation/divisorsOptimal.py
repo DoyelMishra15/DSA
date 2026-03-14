1fa86182fd13dcb8b6d5bc0d336f626a738da6ae
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-03-14 22:18:32 +0530

    Add function to print prime divisors of a number
    Implement a function to print all divisors of a given number, filtering for prime divisors.

---
+++ BitManipulation/divisorsOptimal.py
@@ -0,0 +1,20 @@
+#Print all Divisors of a given Number
+
+def isprime(n):
+  if n<=1:
+      return False
+  for i in range(2,int(n**0.5)+1):
+    if n%i==0:
+      return False
+  return True
+
+n=int(input())
+s=set()
+for i in range(1,int(n**0.5)+1):
+  if isprime(i) and n%i==0:
+    if i not in s:
+      s.add(i)
+    if isprime(n//i):
+      if n//i not in s:
+        s.add(n//i)
+print(list(s))
