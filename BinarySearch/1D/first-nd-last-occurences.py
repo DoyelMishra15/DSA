8ef400148ee0fceca1acae00bf0f13bcc2bd653f
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-03-22 20:31:46 +0530

    Create first-nd-last-occurences.py

---
+++ BinarySearch/1D/first-nd-last-occurences.py
@@ -0,0 +1,26 @@
+#first nd last occurences
+arr=list(map(int,input().split()))
+x=int(input())
+n=len(arr)
+ans=n
+l,r=0,n-1
+while l<=r:
+    mid=(l+r)//2
+    if arr[mid]>=x:
+        ans=mid
+        r=mid-1
+    else:
+        l=mid+1
+ans1=n
+l,r=0,n-1
+while l<=r:
+    mid=(l+r)//2
+    if arr[mid]>x:
+        ans1=mid
+        r=mid-1
+    else:
+        l=mid+1
+if ans == n or arr[ans] != x:
+    print([-1, -1])
+else:
+  print([ans, ans1 - 1])
