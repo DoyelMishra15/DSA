871a3b7f6a5f2037a68c4cef79660f40d0005c34
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-03-22 21:54:52 +0530

    Add function to count rotations in a rotated array
    Implement a function to count array rotations.

---
+++ BinarySearch/1D/array_rotated.py
@@ -0,0 +1,11 @@
+#How many times Array is rotated
+arr=list(map(int,input().split()))
+n=len(arr)
+l,r=0,n-1
+while l<r:
+    mid=(l+r)//2
+    if arr[mid]>arr[r]:
+        l=mid+1
+    else:
+        r=mid
+print(n-l)
