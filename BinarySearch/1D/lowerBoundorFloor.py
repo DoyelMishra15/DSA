7620ad4f06b196a2de2fb4d3ac2a7553fb75b7dc
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-03-22 19:18:48 +0530

    Add lower bound function using binary search
    Implement binary search to find the lower bound of a given value in a list.

---
+++ BinarySearch/1D/lowerBoundorFloor.py
@@ -0,0 +1,14 @@
+#lower bound
+arr=list(map(int,input().split()))
+x=int(input())
+n=len(arr)
+l,r=0,n-1
+ans=0
+while l<=r:
+  mid=(l+r)//2
+  if arr[mid]>=x:
+    ans=mid
+    r=mid-1
+  else:
+    l=mid+1
+print(ans)
