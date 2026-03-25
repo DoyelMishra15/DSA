46f3f71afd888f00d52b01dd25fabc2c245159b3
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-03-25 09:35:00 +0530

    Add rotated array search implementation
    Implement binary search for an element in a rotated sorted array.

---
+++ BinarySearch/1D/rotatedArraySearch.py
@@ -0,0 +1,25 @@
+#Search Element in a Rotated Sorted Array
+
+arr=list(map(int,input().split()))
+x=int(input())
+
+def ans(arr,x):
+  l,r=0,len(arr)-1
+  while l<=r:
+    mid=(l+r)//2
+    if arr[mid]==x:
+      return mid
+    if arr[l]<=arr[mid]:
+      if arr[l] <= x < arr[mid]:
+        r =mid-1
+      else:
+        l=mid+1
+    else:
+      if arr[mid] < x <= arr[r]:
+        r =mid-1
+      else:
+        l=mid+1
+  else:
+    return -1
+    
+print(ans(arr,x))
