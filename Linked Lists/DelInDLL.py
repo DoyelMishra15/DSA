26d5b99fc82b54358aae2c0bfc106ff74c20e47f
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-04-22 14:19:10 +0530

    Create DelInDLL.py

---
+++ Linked Lists/DelInDLL.py
@@ -0,0 +1,32 @@
+class Node:
+  def __init__(self,data):
+    self.data=data
+    self.prev=None
+    self.next=None
+
+def delAtStart(head):
+  if head==None or not head.next:
+    return None
+  head=head.next
+  head.prev=None
+  return head
+
+head = Node(1)
+
+# Node 2
+n2 = Node(2)
+head.next = n2
+n2.prev = head
+
+# Node 3
+n3 = Node(3)
+n2.next = n3
+n3.prev = n2
+
+# Node 4
+n4 = Node(4)
+n3.next = n4
+n4.prev = n3
+print(head.data)
+head=delAtStart(head)
+print(head.data)
