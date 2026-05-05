4c331c1a3b2762636c3108146dccfcc08bb29375
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-05-05 19:43:21 +0530

    Update LCS.py

--- Array/Med Problems/LCS.py
+++ Array/Med Problems/LCS.py
@@ -1,13 +1,17 @@
 arr=[102,4,100,1,101,3,2,1,1]
-mlen=1 
-arr=sorted(set(arr))
-print(arr)
-curr=1
-for i in range(len(arr)-1):
-  if arr[i]+1==arr[i+1]:
-    curr+=1 
-  else:
-    mlen=max(curr,mlen)
-    curr=1
-mlen=max(curr,mlen)
-print(mlen)
+def lcs(arr):
+  n=len(arr)
+  if n==0:
+    return 0
+  mlen=1 
+  swt=set(arr)
+  for i in swt:
+    if i-1 not in swt:
+      cnt=1 
+      x=i 
+      while x+1 in swt:
+        cnt+=1 
+        x+=1
+      mlen=max(cnt,mlen)
+  return mlen
+print(lcs(arr))
