6c1389c0415dc2b2a830f3eb20623ecfeec4731f
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-05-08 18:36:29 +0530

    Update NextGreaterElement2.py

--- Stack and Queue/VVVIP/NextGreaterElement2.py
+++ Stack and Queue/VVVIP/NextGreaterElement2.py
@@ -12,3 +12,18 @@ class Solution:
         return res
 
 
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        res = [-1] * n
+        s = []
+        for i in range(2*n-1, -1, -1):
+            while s and s[-1] <= nums[i%n]:
+                s.pop()
+            if i<n:
+                if s:
+                    res[i] = s[-1]
+                else:
+                    res[i]=-1
+            s.append(nums[i%n])
+        return res
