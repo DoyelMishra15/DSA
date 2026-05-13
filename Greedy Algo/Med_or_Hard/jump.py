e9160ac7081bea62d839e2367e6d4093ae66baa0
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-05-13 16:23:26 +0530

    Update jump.py

--- Greedy Algo/Med_or_Hard/jump.py
+++ Greedy Algo/Med_or_Hard/jump.py
@@ -1,3 +1,5 @@
+#Bruteforce
+
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n=len(nums)
@@ -9,3 +11,18 @@ class Solution:
                 mini=min(mini,rec(idx+i,jump+1))
             return mini
         return rec(0,0)
+
+#optimal
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n=len(nums)
+        l=r=0
+        jump=0
+        while r<n-1:
+            farthest=0
+            for i in range(l,r+1):
+                farthest=max(i+nums[i],farthest)
+            l=r+1
+            r=farthest
+            jump+=1
+        return jump
