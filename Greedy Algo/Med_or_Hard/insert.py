3ec76f6b60c8f8d67ac7b0e6e00ef3446fb7909b
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-05-14 18:16:33 +0530

    Update insert.py

--- Greedy Algo/Med_or_Hard/insert.py
+++ Greedy Algo/Med_or_Hard/insert.py
@@ -1,1 +1,19 @@
-jj
+#brute
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newIntervals: List[int]) -> List[List[int]]:
+        n=len(intervals)
+        res=[]
+        i=0
+        while i<n and intervals[i][1]<newIntervals[0]:
+            res.append(intervals[i])
+            i+=1
+        while i<n and intervals[i][0]<=newIntervals[1]:
+            newIntervals[0]=min(newIntervals[0],intervals[i][0])
+            newIntervals[1]=max(newIntervals[1],intervals[i][1])
+            i+=1
+        res.append(newIntervals)
+        while i<n:
+            res.append(intervals[i])
+            i+=1
+        return res
