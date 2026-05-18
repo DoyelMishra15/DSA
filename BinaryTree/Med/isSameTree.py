a93e3728a42e9595bac186949e2f5b30dac49258
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-05-18 15:30:52 +0530

    Improve formatting and readability of isSameTree
    Refactor isSameTree function for improved readability.

--- BinaryTree/Med/isSameTree.py
+++ BinaryTree/Med/isSameTree.py
@@ -10,6 +10,10 @@ class Solution:
             return True
         if p is None or q is None:
             return False
-        if p.val!=q.val:
+        if p.val != q.val:
             return False
-        return (isSameTree(p.left,q.left) and isSameTree(p.right,q.right))
+        return (
+            self.isSameTree(p.left, q.left)
+            and
+            self.isSameTree(p.right, q.right)
+        )
