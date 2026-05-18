6b8730dc5068a0d5abd13f4a97521d9a9acd9ca8
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-05-18 18:57:42 +0530

    Create findMax.py

---
+++ Binary Search Tree/concepts/findMax.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def findMax(self, root):
+
+        while root.right:
+            root = root.right
+
+        return root.val
