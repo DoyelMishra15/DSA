e155fb24b01b66f8888eafd1233030284b7efb44
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-06-03 17:51:34 +0530

    Update frogjumpk.py

--- DP/1D/frogjumpk.py
+++ DP/1D/frogjumpk.py
@@ -1,1 +1,26 @@
+heights=[10, 5, 20, 0, 15]#list((map(int,input().split(',')))
+k=2#int(input())
+heights = [10, 5, 20, 0, 15]
+k = 2
 
+n = len(heights)
+dp = [-1] * n
+
+def mem(i):
+    if i == 0:
+        return 0
+
+    if dp[i] != -1:
+        return dp[i]
+
+    ans = float('inf')
+
+    for j in range(1, k + 1):
+        if i - j >= 0:
+            cost = mem(i - j) + abs(heights[i] - heights[i - j])
+            ans = min(ans, cost)
+
+    dp[i] = ans
+    return ans
+
+print(mem(n - 1))
