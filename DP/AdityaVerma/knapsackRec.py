694629de1215415b0c96ff54632ed8251d8977e5
DOYEL MISHRA <143923639+DoyelMishra15@users.noreply.github.com>
2026-07-14 21:27:09 +0530

    Update knapsackRec.py

--- DP/AdityaVerma/knapsackRec.py
+++ DP/AdityaVerma/knapsackRec.py
@@ -1,15 +1,19 @@
 wt=[1,3,4,5]
 val=[1,4,5,7]
 w=7
+n=len(wt)
 ans=-2**31
 def rec(i,wt,val,curr,v):
     global ans
     if curr > w:
         return
-    if i>=len(wt):
+    if i>=n:
         ans=max(ans,v)
         return
-    p=rec(i+1,wt,val,curr+wt[i],v+val[i])
-    np=rec(i+1,wt,val,curr,v)
-rec(0,wt,val,0,0)
-print(ans)
+    rec(i+1,wt,val,curr+wt[i],v+val[i])
+    rec(i+1,wt,val,curr,v)
+if n==0 or w==0:
+    print(0)
+else:
+    rec(0,wt,val,0,0)
+    print(ans)
